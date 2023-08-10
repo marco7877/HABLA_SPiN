4707a68b2af48f3f197bfc9d8e789b200d50c4d7
marco7877 <marcoantonio7877@gmail.com>
2023-08-10 15:48:23 +0200

    legible code sections

--- headMask.py
+++ headMask.py
@@ -5,7 +5,7 @@ import argparse
 Note: You should have installed AFNI in order for this code to work. Remember to run MPRAGE before,
 because we will search for files ended in rebiased_clean.nii.gz
 '''
-
+####### Arguments #############################################################
 parser=argparse.ArgumentParser(
         description="Creates a whole head mask from T1 uni-clean")
 parser.add_argument("--bids_dir", default=None, type=str,
@@ -24,21 +24,21 @@ dilate=str(args.dilate)
 output_dir=args.output_dir
 # Here we could have a condition to check if the script has already been run and the files are there.
 # Im skipping this for now
-#reading target files
+####### Reading files #########################################################
 source_T1= sorted([os.path.join(root, x) for root,dirs,files in os.walk(bids_dir) 
                    for x in files if x.endswith("uniclean_T1w.nii.gz")])
 print(source_T1)
-#making output file name
+####### Output names/dir ######################################################
 output_mask= [niifti.replace("uniclean_T1w.nii.gz", "whead_mask.nii.gz") and
               niifti.replace("anat/", output_dir)
               for niifti in source_T1]
-### checking if output directory exist
+# checking if output directory exist
 filenames=[ directory.partition("anat/")[0]+output_dir for directory in source_T1]
 for directory in filenames:
     if not os.path.exists(directory):
         os.mkdir(directory)
 del filenames
-
+####### Head mask creation ####################################################
 print("Calculating whole head mask")
 for i in range(len(source_T1)):
     os.system("3dcalc" + " -a " + source_T1[i] + " -expr 'step(a-60)' " +
