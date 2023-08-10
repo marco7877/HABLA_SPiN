d8ca154e9150bc57f2f1e2a3df625b22e747346d
marco7877 <marcoantonio7877@gmail.com>
2023-08-10 16:09:35 +0200

    output_dir argument added

--- realignment.py
+++ realignment.py
@@ -11,10 +11,19 @@ parser=argparse.ArgumentParser(
         inputs are sbref and bold files. 
         Generate a motion correction matrix from sbref echo 1,
         then applies it to the remaining echoes""")
-parser.add_argument("--bids_dir", default=None, type=str,help="Full path to the BIDS directory ")
-parser.add_argument("--echoes", default= None, type=int,help="Number of echoes")
+parser.add_argument("--bids_dir", default=None, type=str,
+                    help="Full path to the BIDS directory ")
+parser.add_argument("--echoes", default= None, type=int,
+                    help="Number of echoes")
+parser.add_argument("--output_dir",default="func_preproc/", type =str,
+                    help ="""Directory to store output at.
+                    Default is func_preproc:
+                        -anat
+                        -func
+                        -func_preproc""")
 args = parser.parse_args()
 bids_dir=args.bids_dir
+output_dir=args.output_dir
 #bids_dir= "/bcbl/home/public/MarcoMotion/Habla_restingState"#to debbug
 echoes=args.parse_args()
 #echoes=4#to debbug
@@ -25,30 +34,40 @@ echoes=args.parse_args()
 source_sbref= sorted([os.path.join(root, x) 
                       for root,dirs,files in os.walk(bids_dir) 
                       for x in files if x.endswith("echo-1_sbref.nii.gz")])
-#making output file name
-filenames=[x.split("/")[-1] for x in source_sbref]
-file_len=[len(x) for x in filenames]
-filenames=[x.replace(".nii.gz","") for x in filenames]
+####### Output names/dir ######################################################
+output_filerealign=[x.split("/")[-1] for x in source_sbref]
+file_len=[len(x) for x in output_filerealign]
+output_filerealign=[x.replace(".nii.gz","") for x in output_filerealign]
+# checking if output directory exist
+
+filenames=[ directory.partition("anat/")[0]+output_dir
+           for directory in source_sbref]
+for directory in filenames:
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+del filenames
 ####### Matrix motion correction from sbref echo n ############################
 print(f"Generating {len(source_sbref)} reference image for motion correction (mean based)")
 for i in range(len(source_sbref)):
     os.chdir(source_sbref[i][0:-file_len[i]])
     os.system("3dvolreg -overwrite -Fourier -base " + source_sbref[i]+ 
-              " -1Dfile " + filenames[i] + "_mcf.1D " +
-              "-1Dmatrix_save "+ filenames[i] + "_mcf.aff12.1D -prefix " +
-              filenames[i] + "_mcf.nii.gz "+ filenames[i] + ".nii.gz")
-    #os.system("3dTstat -mean -prefix "+ filenames[i]+"mean_ref "+ source_sbref[i]) 
+              " -1Dfile " + output_filerealign[i] + "_mcf.1D " +
+              "-1Dmatrix_save "+ output_filerealign[i] + "_mcf.aff12.1D -prefix " +
+              output_filerealign[i] + "_mcf.nii.gz "+ output_filerealign[i] + ".nii.gz")
 ####### Realigning all echos ##################################################
 print("Realigning remaining echoes ")
 realign_ref= sorted([os.path.join(root, x) 
                       for root,dirs,files in os.walk(bids_dir) 
                       for x in files if x.endswith("_mcf.aff12.1D")])
-filenames_out=[x.replace("1_sbref.nii.gz","") for x in source_sbref]
+filenames_out=[x.replace("1_sbref.nii.gz","") and x.replace("anat/","func_preproc")
+               for x in source_sbref]
+# TODO: erase below line when rec-magnitude and rec-phase added in all niifti files
 filenames_out=[x.replace("_rec-magnitude","") for x in filenames_out]
+
 for i in range(len(source_sbref)):
     target_dir=source_sbref[i][0:-file_len[i]]
     os.chdir(target_dir)
-    for echo in range(1, echoes):#indentation starts in 0
+    for echo in range(echoes):#indentation starts in 0
         tmp_filename=filenames_out[i]+str(echo+1)#skipping first echo
         os.system("3dAllineate -overwrite -base "+source_sbref[i]+
                   " -final cubic -1Dmatrix_apply "+ realign_ref[i]+
