1b4abc0daef47a686f36aaa1dd5da6a40733d3ed
marco7877 <marcoantonio7877@gmail.com>
2023-08-11 16:08:48 +0200

    Changed source bids directory

--- headMask.sh
+++ headMask.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This uses the headMask.py script to generate whole head mask
-python headMask.py --bids_dir /bcbl/home/public/MarcoMotion/Habla_RE
+python headMask.py --bids_dir /bcbl/home/public/MarcoMotion/Habla_restingState
--- mprageize.sh
+++ mprageize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # we are using rys script mprageize_bids.py 
-python mprageize_bids.py --bids_dir /bcbl/home/public/MarcoMotion/Habla_RE  --mprageize_dir /bcbl/home/home_g-m/mflores/mytoolboxes/3dMPRAGEise --overwrite True
+python mprageize_bids.py --bids_dir /bcbl/home/public/MarcoMotion/Habla_restingState  --mprageize_dir /bcbl/home/home_g-m/mflores/mytoolboxes/3dMPRAGEise --overwrite True
