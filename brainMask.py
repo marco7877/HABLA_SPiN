31efa93f95153daaa7fe3d254769f8ebc7a4464d
marco7877 <marcoantonio7877@gmail.com>
2023-08-14 15:19:30 +0200

    debbuged to properly 3dskullstrip & 3dcalc mask & 3dToutcount

--- brainMask.py
+++ brainMask.py
@@ -12,10 +12,10 @@ because we will search for files ended in rebiased_clean.nii.gz
 ####### DEBUGG
 #bold_phase_ext="_part-phase_bold"
 #bold_ext="_bold"
-echoes=4
-bids_dir="/bcbl/home/public/MarcoMotion/Habla_restingState/"
-output_dir="func_preproc/"
-dilate= 1
+#echoes=4
+#bids_dir="/bcbl/home/public/MarcoMotion/Habla_debbug"
+#output_dir="func_preproc/"
+#dilate= 1
 #drop_vol=10
 #drop_noise=3
 ####### TODO: Add despiking and trimming as non-default options
@@ -32,17 +32,30 @@ parser.add_argument("--output_dir",default="func_preproc/", type =str,
                         -anat
                         -func
                         -func_preproc""")
+parser.add_argument("--despike", default=False,type=bool,
+                    help="Despike data? Default is False")                        
+parser.add_argument("--sliceTimming", default=False,type=bool,
+                    help="""Slice timme data? Default is False
+                    WARNING: 
+                        THIS FEATURE IS IN DEPLOYMENT. IT DOESN'T NOT WORK YET""")                        
 args = parser.parse_args()
 bids_dir=args.bids_dir
 dilate=str(args.dilate)
 output_dir=args.output_dir
+despike=args.despike
+sliceTimming=args.sliceTimming
 # Here we could have a condition to check if the script has already been run and the files are there.
 # Im skipping this for now
 ####### Reading files #########################################################
 # TODO: change echo-1_sbref.nii.gz to echo-1_part-mag_sbref.nii.gz
 source_sbref= sorted([os.path.join(root, x) 
                       for root,dirs,files in os.walk(bids_dir) 
-                      for x in files if x.endswith("echo-1_sbref.nii.gz")])
+                      for x in files if x.endswith("echo-1_part-mag_sbref.nii.gz")])
+tcount_bold= [ niifti.replace("sbref","bold") for niifti in source_sbref]
+tcount_bold= [ niifti.replace(".nii.gz","") for niifti in tcount_bold]
+tcount_bold= [ niifti.replace("func",output_dir)for niifti in tcount_bold]
+preproc_bold= [niifti.replace("func/",output_dir)for niifti in source_sbref]
+preproc_bold= [niifti.replace("sbref","bold_dsd") for niifti in preproc_bold]
 ####### Output names/dir ######################################################
 output_msk= [niifti.replace("func/", output_dir)  for niifti in source_sbref]
 output_mask_surf= [niifti.replace("sbref", "brain_mask_surf") for niifti in output_msk]
@@ -54,8 +67,8 @@ for directory in filenames:
         os.mkdir(directory)
 del filenames
 ####### Head mask creation ####################################################
-print("Calculating whole head mask")
 for i in range(len(source_sbref)):
+    print("Calculating brain mask")
     os.system("3dSkullStrip -input "+ source_sbref[i]+
               " -prefix "+ output_mask_surf[i]+
               " -use_skull -no_avoid_eyes -mask_vol -overwrite")
@@ -64,7 +77,20 @@ for i in range(len(source_sbref)):
     os.system("3dmask_tool -input "+output_mask[i]+" -prefix "+output_mask[i]+
               " -fill_holes -dilate_inputs " + " +" +
                         dilate +" -" + dilate+ " -overwrite")
-    # TODO: add outlier removal
-    #os.system("3dToutcount -mask "output_mask[i]" -fraction -polort 5 -legendre "${funcsource}.nii.gz" > "${func}_outcount.1D")
+    os.system("3dToutcount -mask "+output_mask[i]+
+              " -fraction -polort 5 -legendre "+preproc_bold[i]+
+              " > "+tcount_bold[i]+"_outcount.1D")
+if despike == True:
+    for i in range(len(source_sbref)):
+        print("Despiking Data")
+        os.system("3dDespike -prefix "+tcount_bold[i]+"_dsk.nii.gz "+
+                  preproc_bold[i]+".nii.gz -overwrite")
+# TODO: add slice timming     
+#if sliceTimming == True:
+ #   for i in range(len(source_sbref)):
+  #      print("Slice timming Data")
+   #     os.system("3dTshift -Fourier -prefix "tcount_bold[i]"_si.nii.gz" -tpattern "${siot}" -overwrite "${funcsource}.nii.gz" -overwrite)
+
+    
     
 
