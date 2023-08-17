c7a990a93e1468e5e5737f9dd26561c21efcc7ff
marco7877 <marcoantonio7877@gmail.com>
2023-08-17 16:13:01 +0200

    Headmask transformed from T1 to sbref space for all tasks

--- headMask.py
+++ headMask.py
@@ -9,6 +9,15 @@ import argparse
 Note: You should have installed AFNI in order for this code to work. Remember to run MPRAGE before,
 because we will search for files ended in rebiased_clean.nii.gz
 '''
+####### DEBUGG
+#bold_phase_ext="_part-phase_bold"
+#bold_ext="_part-phase_bold"
+#echoes=4
+bids_dir="/bcbl/home/public/MarcoMotion/Habla_restingState/"
+output_dir="func_preproc/"
+#drop_vol=10
+#drop_noise=3
+####### Arguments #############################################################
 ####### Arguments #############################################################
 parser=argparse.ArgumentParser(
         description="Creates a whole head mask from T1 uni-clean")
@@ -43,9 +52,9 @@ if filt_pattern != None:
     source_T1=sorted([directory for directory in source_T1
                   if filt_pattern in directory])
 ####### Output names/dir ######################################################
-output_mask= [niifti.replace("uniclean_T1w.nii.gz", "whead_mask.nii.gz") and
-              niifti.replace("anat/", output_dir)
+output_mask= [niifti.replace("uniclean_T1w.nii.gz", "whead_mask.nii.gz")
               for niifti in source_T1]
+output_mask= [niifti.replace("anat/", output_dir)for niifti in output_mask]
 # checking if output directory exist
 filenames=[ directory.partition("anat/")[0]+output_dir for directory in source_T1]
 for directory in filenames:
@@ -55,9 +64,39 @@ del filenames
 ####### Head mask creation ####################################################
 print("Calculating whole head mask")
 for i in range(len(source_T1)):
-    os.system("3dcalc" + " -a " + source_T1[i] + " -expr 'step(a-3)' " +
-              " -prefix " + output_mask[i])
+    os.system("3dcalc" + " -a " + source_T1[i] + " -expr 'astep(a,70)' " +
+              " -prefix " + output_mask[i] + " -overwrite")
     os.system("3dmask_tool" + " -fill_holes -dilate_inputs " + " +" +
-              dilate +" -" + dilate+ " -input "  + output_mask[i] +
+              str(dilate) +" -" + str(dilate)+ " -input "  + output_mask[i] +
               " -prefix " + output_mask[i] + " -overwrite" )
-
+####### Head mask trasnformation ##############################################    
+source_sbref= sorted([os.path.join(root, x) 
+                      for root,dirs,files in os.walk(bids_dir) 
+                      for x in files if x.endswith("echo-1_part-mag_sbref.nii.gz")])
+sbref_names=list(set([directory.partition("func/")[-1]
+                        for directory in source_sbref]))
+subjects=sorted(list(set([file.split("_")[0] for file in sbref_names])))
+tasks=sorted(list(set([file.split("_")[2] for file in sbref_names])))
+del sbref_names
+output_mask_name=[niifti.partition("acq-")[0] for niifti in output_mask]
+for subject in subjects:
+    target_sbref=[directory for directory in source_sbref
+                  if subject in directory]
+    target_T1=[directory for directory in source_T1
+                  if subject in directory]
+    target_mask=[directory for directory in output_mask
+                  if subject in directory]
+    target_mask_out=[directory for directory in output_mask_name
+                  if subject in directory]
+    os.chdir([target_mask_out[0].partition(output_dir)[0]+output_dir][0])
+    for i in range(len(tasks)):
+        tmp_mask_out=target_mask_out[0]+tasks[i]+"_acq-whead_mask.nii.gz"
+        tmp_maskAlign_ref=target_mask_out[0]+tasks[i]+"_acq-whead_mask_mcf.aff12.1D"
+        os.system("align_epi_anat.py -epi "+ target_sbref[0]+
+                  " -anat "+target_T1[0]+ " -epi_base mean -save_Al_in")
+        os.system("3dAllineate -overwrite -base "+target_sbref[i]+
+                  " -final cubic -1Dmatrix_apply "+
+                  subject+"_ses-1_acq-uniclean_T1w_al_mat.aff12.1D"+
+                  " -prefix "+ tmp_mask_out+
+                  " "+target_mask[0])
+        os.system("rm *.1D *.BRIK *.HEAD")
