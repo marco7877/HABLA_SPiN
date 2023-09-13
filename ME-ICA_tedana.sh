6cc20e4f3ccaad86d3526205a26e1b730050dfb9
marco7877 <marcoantonio7877@gmail.com>
2023-09-13 11:23:39 +0200

    changes

--- ME-ICA_tedana.sh
+++ ME-ICA_tedana.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-python ME-ICA_tedana.py --bids_dir /bcbl/home/public/MarcoMotion/Habla_restingState/ --echoes 4 --TE "11 28 45 61" --output_dir ME-ICA_nordic/ --preproc_bold_ext bold_nordic --nordic True 
+python ME-ICA_tedana.py --bids_dir /bcbl/home/public/MarcoMotion/Habla_restingState/ --echoes 4 --TE "11 28 45 61" --output_dir ME-ICA_nordic/ --preproc_bold_ext bold_mcf_al --mask_ext acq-whead_mask --nordic True 
