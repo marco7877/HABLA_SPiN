38dc16fb48fe90c45be60710adcfcbf4a08d4596
Marco Flores-Coronado <56403434+marco7877@users.noreply.github.com>
2024-02-16 16:16:59 +0100

    Delete unifize.sh

--- unifize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in $(seq 1 5);
-do
-	3dUnifize -input /bcbl/home/public/MarcoMotion/Habla_restingState/sub-00${i}/ses-1/anat/sub-00${i}_ses-1_acq-uniclean_T1w.nii.gz -prefix /bcbl/home/public/MarcoMotion/Habla_restingState/sub-00${i}/ses-1/anat/sub-00${i}_ses-1_acq-uniclean_T1w.nii.gz -overwrite
-done
-
