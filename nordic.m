2d889cdce7169e30ad29d267fea55a463082ea3d
Marco Flores-Coronado <56403434+marco7877@users.noreply.github.com>
2024-02-16 16:20:59 +0100

    Delete nordic.m

--- nordic.m
+++
@@ -1,7 +0,0 @@
-addpath(genpath("/bcbl/home/home_g-m/mflores/mytoolboxes/NORDIC_raw/"));
-cd '/bcbl/home/public/MarcoMotion/scripts/HABLA_SPiN/';
-ARG.temporal_phase=1;
-ARG.phase_filter_width=10;
-ARG.save_add_info=1;
-ARG.DIROUT='target';
-NIFTI_NORDIC('bold_mag','bold_phase','fn_out',ARG);
